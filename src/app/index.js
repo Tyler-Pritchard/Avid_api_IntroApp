da8170b18031794f6589f50bb63f5713a73c1f47
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-01-15 01:49:01 -0800

    init

---
+++ src/app/index.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2019-2021 by Avid Technology, Inc.
+ */
+import { getLocalization } from './localization';
+import l10nData from '../l10n/lang.en.json';
+import * as styles from './index.scss';
+
+export default class ApplicationContainer {
+    getTitle() {
+        return 'Your first AVID app title';
+    }
+
+    render(element) {
+        element.classList.add(styles.test);
+        element.innerHTML = getLocalization(l10nData)('example-plugin-message');
+    }
+}
